d7f58df388c63c2a94372579acead571b5a83c5e
François Georgy <francois.georgy@gmail.com>
2018-08-16 16:11:15 +0200

    todo

--- src/App.js
+++ src/App.js
@@ -1,21 +1,25 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
+// import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div className="App">
+                <header className="App-header">
+                    {/*<img src={logo} className="App-logo" alt="logo"/>*/}
+                    <h1 className="App-title">Music Calculators</h1>
+                </header>
+                <div>
+                    <div>key</div>
+                    <div>scale</div>
+                    <div>notes</div>
+                    <div>semitones</div>
+                    <div>intervals</div>
+                </div>
+            </div>
+        );
+    }
 }
 
 export default App;
